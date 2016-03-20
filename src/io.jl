bcd815a32d6804e2c536945cdc2f3ee77a7c9de9
Oliver Schulz <oschulz@mpp.mpg.de>
2016-03-20 13:44:06 +0100

    Added open_decompressed

--- src/io.jl
+++ src/io.jl
@@ -2,6 +2,8 @@
 
 import Base: read, write
 
+export open_decompressed
+
 
 immutable BigEndianIO{IOType <: IO}
     io::IOType
@@ -12,3 +14,21 @@ write(s::BigEndianIO, x::Integer) =
 
 read{T <: Integer}(s::BigEndianIO, ::Type{T}) =
     bswap(read(s.io, T))
+
+
+open_decompressed(filename::AbstractString) = begin
+    if endswith(filename, ".gz")
+        open(`gzip -d -c $filename`, "r", STDOUT)[1]
+    elseif endswith(filename, ".bz2")
+        try
+            open(`pbzip2 -d -c $filename`, "r", STDOUT)[1]
+        catch
+            info("pbzip2 doesn't seem to be available, falling back to standard bzip2")
+            open(`bzip2 -d -c $filename`, "r", STDOUT)[1]
+        end
+    elseif endswith(filename, ".xz")
+        open(`xz -d -c $filename`, "r", STDOUT)[1]
+    else
+         open(filename, "r")
+    end
+end
