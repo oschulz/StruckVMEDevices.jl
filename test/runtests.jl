9a1a96bb5512951636d9afd69fb0f12cd745d918
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-22 10:07:08 +0200

    Use Compat.Test instead of Base.Test for Julia v0.7 compatibility

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 # This file is a part of SIS3316.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package SIS3316" begin
+import Compat.Test
+Test.@testset "Package SIS3316" begin
     include.([
         "io.jl",
         "iter_view.jl",
