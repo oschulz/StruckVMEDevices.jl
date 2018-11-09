f2bd7ecd0c5eeb188f2f82c32fc6158e603f742c
Lukas <lhauert@mpp.mpg.de>
2018-11-09 14:40:06 +0100

    Base.test -> Compat.test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,10 @@
 # This file is a part of SIS3316.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package SIS3316" begin
-    include.([
-        "io.jl",
-        "iter_view.jl",
-        "evtformat.jl",
-        "sortevents.jl",
-        "filters.jl",
-    ])
+import Compat.Test
+Test.@testset "Package SIS3316" begin
+	include("../src/io.jl")
+	include("../src/iter_view.jl")
+	include("../src/evtformat.jl")
+	include("../src/sortevents.jl")
+	include("../src/filters.jl")
 end
